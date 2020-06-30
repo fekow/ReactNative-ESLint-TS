16c146074eea2373371ff6d3bab6f35376bb7967
Fernando Binkowski <binkowskideandrade@gmail.com>
2020-06-30 19:15:50 -0300

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+interface Props {
+  
+}
+
+const App = (props: Props) => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  )
+}
+
+export default App
